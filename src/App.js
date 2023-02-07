3d574ad0ebe121af8bc0de4b6412a4baac9124a3
Filip Bolčić <filip.bolcic@arsfutura.co>
2023-02-07 17:01:05 +0100

    feat: add base content to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import styles from "./App.module.css";
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <h1 className={styles.heading}>Hello World</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
