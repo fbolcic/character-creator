564bba2f07b6f49fc6098759a6f7ef3f6e7bc9dd
Filip Bolčić <filip.bolcic@arsfutura.co>
2023-03-04 16:40:11 +0100

    feat: generate image from character inputs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { useState } from "react";
-import styles from "./App.module.css";
+
+import { useGeneratedImage } from "./hooks";
+import { generatePromptFromCharacter } from "./utils";
 import { Header } from "./components/Header";
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
 import { Radio } from "./components/Radio";
 import { Textarea } from "./components/Textarea";
+import { Separator } from "./components/Separator";
+import { Image } from "./components/Image";
+
+import styles from "./App.module.css";
 
 function App() {
   const [name, setName] = useState("");
@@ -12,7 +18,14 @@ function App() {
   const [gender, setGender] = useState("female");
   const [description, setDescription] = useState("");
 
+  const { getImageUrl, imageUrl, isIdle, isLoading, isError, isSuccess } = useGeneratedImage();
+
   const character = { name, superpower, gender, description };
+
+  function handleGenerate() {
+    const prompt = generatePromptFromCharacter(character);
+    getImageUrl(prompt);
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -24,8 +37,10 @@ function App() {
             <Input label="Superpower" value={superpower} onChange={setSuperpower} />
             <Radio label="Gender" options={["male", "female"]} value={gender} onChange={setGender} />
             <Textarea label="Description" value={description} onChange={setDescription} />
-            <Button text="Generate" onClick={() => console.log("Character: ", character)} />
+            <Button text="Generate" onClick={handleGenerate} />
           </div>
+          <Separator />
+          <Image imageUrl={imageUrl} isIdle={isIdle} isLoading={isLoading} isError={isError} isSuccess={isSuccess} />
         </div>
       </div>
     </div>
